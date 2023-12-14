1d615c6b57f616faa7ec9df97e7bcdb66b1346a2
Normaljun95 <jsj950629@gmail.com>
2023-12-14 11:03:47 +0900

    ✨Feat: user model

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const app = express();
 
 //배포 시에는 끌 것
 app.use(cors());
+
+const DB_URL = process.env.ATLAS_URL;
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+db.on("connected", () => {
+    console.log("DB 연결 성공");
+});
+db.on("error", (error) => {
+    console.log("DB 연결 실패");
+});
---
+++ server/models/user-model.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+    {
+        user_email: {
+            type: String,
+            required: true,
+        },
+        user_id: {
+            type: String,
+            required: true,
+        },
+        nickname: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        image_url: {
+            type: String,
+            default: "defaultImage.png",
+        },
+    },
+    {
+        collection: "users",
+        timestamps: true,
+    }
+);
+
+const User = model("users", UserSchema);
+export { User };
