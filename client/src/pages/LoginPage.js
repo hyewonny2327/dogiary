acfb7af037b505051c4c5fe56d547c7c1d1d4d78
Song eunji <ckacl369@gmail.com>
2023-12-15 04:02:43 +0900

    :art: Ui 위치 작업
    버튼은 아직 입니다.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,12 +1,12 @@
 // 로그인 페이지 컴포넌트
 import React, { useState } from 'react';
 import { LogoBar, NavBar } from '../components/common/Header';
-import { LongColoredBtn, LongStrokedBtn, SmallBtn } from '../components/common/Buttons';
-import { ContainerBox, InputBox } from '../components/common/Boxes';
-import { Modal } from '../components/common/Modal';
+import { LongColoredBtn, LongStrokedBtn } from '../components/common/Buttons';
+import { ContainerBox as StyledContainerBox, InputBox } from '../components/common/Boxes';
+import styled from 'styled-components';
 
-// YourFormComponent를 밖으로 빼고 함수 컴포넌트로 선언
-function YourFormComponent() {
+
+function LoginPage() {
   const [user_Id, setUser_Id] = useState('');
   const [password, setPassword] = useState('');
 
@@ -18,73 +18,104 @@ function YourFormComponent() {
     setPassword(e.target.value);
   };
 
-  const submitForm = () => {
+  const submitForm = (e) => {
+    e.preventDefault();
     console.log('Submit form!', user_Id, password);
   };
 
   return (
-    <div>
-      <LogoBar/>
-      <NavBar/>
-      <form>
-      <ContainerBox>
-      <InputBox>
-        <label htmlFor="user_Id">아이디</label>
-        <input
-          className="form-input"
-          value={user_Id}
-          type="text"
-          name="user_Id"
-          id="user_Id"
-          placeholder="아이디 입력"
-          onChange={handleChangeUserId}
-        />
-      </InputBox>
+    <div className="main">
+      <LogoBar />
+      <NavBar />
+      <BigBox className="bigbox">
+        <ContainerBox>
+          <StyledContainerBox>
+            <LoginContainer className="container-box">
+              <div className="text-box">
+                <h3>로그인</h3>
+                <a>로그인을 해서 Dogiary의 서비스를 사용해보세요!</a>
+              </div>
+              <div className="loginBox">
+                <InputBox>
+                  <input
+                    className="form-input"
+                    value={user_Id}
+                    type="text"
+                    name="user_Id"
+                    id="user_Id"
+                    placeholder="아이디 입력"
+                    onChange={handleChangeUserId}
+                  />
+                </InputBox>
 
-      <InputBox>
-        <label htmlFor="password">비밀번호</label>
-        <input
-          className="form-input"
-          value={password}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="비밀번호 입력"
-          onChange={handleChangePassword}
-        />
-        <div>비밀번호는 영문, 숫자 포함 8자 이상</div>
-      </InputBox>
-
-      <div>
-        <button type="submit" onClick={submitForm}>
-          확인
-        </button>
-      </div>
-      </ContainerBox>
-    </form>
+                <InputBox>
+                  <input
+                    className="form-input"
+                    value={password}
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="비밀번호 입력"
+                    onChange={handleChangePassword}
+                  />
+                </InputBox>
+              </div>
+              <div className="btn-box">
+                <LongColoredBtn text={'로그인하기'} className="long-btn" />
+                <LongStrokedBtn text={'회원가입'} className="long-btn" />
+              </div>
+            </LoginContainer>
+          </StyledContainerBox>
+        </ContainerBox>
+      </BigBox>
     </div>
-    
   );
 }
 
-function LoginPage() {
-  const customModalStyle = `
-    width: 371px;
-    height: 500px;
-  `;
-  const containerStyle = `
+const LoginContainer = styled.div`
+  .text-box {
+    margin-left: 35px;
+    margin-right: 35px;
+    margin-bottom: 40px;
+    text-align: center;
+  }
+
+  .form-input {
+    margin-top: 5px;
+    margin-bottom: 20px;
+  }
+
+  .btn-box {
+    margin-top: 50px;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-  `;
+  }
 
-  return (
-    <>
-      {/* YourFormComponent */}
-      <YourFormComponent />
-      {}
-    </>
-  );
-}
+  .long-btn{
+    margin-top: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  }
+
+  display: flex;
+  flex-direction: column;
+  margin-top: 80px;
+  align-items: center;
+`;
+
+const ContainerBox = styled.div`
+  margin-top: 80px;
+`;
+
+const BigBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+
 
 export default LoginPage;
