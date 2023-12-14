a60c2979a85936f3d570a9cdcb1fc8d153e3bb34
Song eunji <ckacl369@gmail.com>
2023-12-14 11:00:24 +0900

    :sparkles: 로그인페이지 연결

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import MyMapPage from './pages/MyMapPage';
+import LoginPage from './pages/LoginPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<MainPage/>}/>
       <Route path='/mapPage' element={<MyMapPage/>}/>
+      <Route path='/LoginPage' element={<LoginPage/>}/>
     </Routes>
   );
 }
