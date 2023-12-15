d69cc00302c24f29de6776aa6c7fe1fff005115c
Song eunji <ckacl369@gmail.com>
2023-12-15 11:37:55 +0900

    :art: 로그인 페이지 ui 배치
    버튼 떨어졌음

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,8 @@
-// 로그인 페이지 컴포넌트
 import React, { useState } from 'react';
 import { LogoBar, NavBar } from '../components/common/Header';
 import { LongColoredBtn, LongStrokedBtn } from '../components/common/Buttons';
-import { ContainerBox as StyledContainerBox, InputBox } from '../components/common/Boxes';
+import { ContainerBox, InputBox } from '../components/common/Boxes';  // Removed StyledContainerBox
 import styled from 'styled-components';
-
 
 function LoginPage() {
   const [user_Id, setUser_Id] = useState('');
@@ -18,10 +16,6 @@ function LoginPage() {
     setPassword(e.target.value);
   };
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    console.log('Submit form!', user_Id, password);
-  };
 
   return (
     <div className="main">
@@ -29,43 +23,41 @@ function LoginPage() {
       <NavBar />
       <BigBox className="bigbox">
         <ContainerBox>
-          <StyledContainerBox>
-            <LoginContainer className="container-box">
-              <div className="text-box">
-                <h3>로그인</h3>
-                <a>로그인을 해서 Dogiary의 서비스를 사용해보세요!</a>
-              </div>
-              <div className="loginBox">
-                <InputBox>
-                  <input
-                    className="form-input"
-                    value={user_Id}
-                    type="text"
-                    name="user_Id"
-                    id="user_Id"
-                    placeholder="아이디 입력"
-                    onChange={handleChangeUserId}
-                  />
-                </InputBox>
+          <LoginContainer className="container-box">
+            <div className="text-box">
+              <h3>로그인</h3>
+              <a>로그인을 해서 Dogiary의 서비스를 사용해보세요!</a>
+            </div>
+            <div className="loginBox">
+              <InputBox>
+                <input
+                  className="form-input"
+                  value={user_Id}
+                  type="text"
+                  name="user_Id"
+                  id="user_Id"
+                  placeholder="아이디 입력"
+                  onChange={handleChangeUserId}
+                />
+              </InputBox>
 
-                <InputBox>
-                  <input
-                    className="form-input"
-                    value={password}
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="비밀번호 입력"
-                    onChange={handleChangePassword}
-                  />
-                </InputBox>
-              </div>
-              <div className="btn-box">
-                <LongColoredBtn text={'로그인하기'} className="long-btn" />
-                <LongStrokedBtn text={'회원가입'} className="long-btn" />
-              </div>
-            </LoginContainer>
-          </StyledContainerBox>
+              <InputBox>
+                <input
+                  className="form-input"
+                  value={password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="비밀번호 입력"
+                  onChange={handleChangePassword}
+                />
+              </InputBox>
+            </div>
+            <div className="btn-box">
+              <LongColoredBtn text={'로그인하기'} className="long-btn" />
+              <LongStrokedBtn text={'회원가입'} className="long-btn" />
+            </div>
+          </LoginContainer>
         </ContainerBox>
       </BigBox>
     </div>
@@ -86,36 +78,27 @@ const LoginContainer = styled.div`
   }
 
   .btn-box {
-    margin-top: 50px;
+    margin-top: 40px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    width:80%;
+    height:90px;
+    justify-content: space-between;
   }
 
-  .long-btn{
-    margin-top: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  }
 
   display: flex;
   flex-direction: column;
   margin-top: 80px;
   align-items: center;
-`;
-
-const ContainerBox = styled.div`
-  margin-top: 80px;
 `;
 
 const BigBox = styled.div`
   display: flex;
-  flex-direction: column;
+  margin-top: 80px;
   align-items: center;
   justify-content: center;
 `;
-
-
 
 export default LoginPage;
