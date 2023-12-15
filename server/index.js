98ed1ff5d499ab14b0c2a5e8440ffa75a19bf856
young <rhsiddlsidhd@nate.com>
2023-12-15 17:17:55 +0900

    :truck: Feat:diaryApi

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./routers/diaryApi.js");
 
+dotenv.config();
+
+const PORT = 5000;
 const app = express();
 
-//배포 시에는 끌 것
 app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const DB_URL = process.env.ATLAS_URL;
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 db.on("connected", () => {
-    console.log("DB 연결 성공");
+  console.log("DB 연결 성공");
 });
 db.on("error", (error) => {
-    console.log("DB 연결 실패");
+  console.log("DB 연결 실패");
 });
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,32 +1,32 @@
 import { model, Schema } from "mongoose";
 
 const UserSchema = new Schema(
-    {
-        user_email: {
-            type: String,
-            required: true,
-        },
-        user_id: {
-            type: String,
-            required: true,
-        },
-        nickname: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        image_url: {
-            type: String,
-            default: "defaultImage.png",
-        },
+  {
+    user_email: {
+      type: String,
+      required: true,
     },
-    {
-        collection: "users",
-        timestamps: true,
-    }
+    user_id: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "defaultImage.png",
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
 );
 
 const User = model("users", UserSchema);
