410fab2ca4db33af03529d7c0b80412457660198
young <rhsiddlsidhd@nate.com>
2023-12-16 14:15:46 +0900

    :goal_net: Error:catch

--- server/index.js
+++ server/index.js
@@ -6,15 +6,13 @@ const cors = require("cors");
 const mapRouter = require("./routers/mapRouter.js");
 const dogRouter = require("./routers/dogRouter.js");
 const diaryRouter = require("./routers/diaryRouter.js");
-const {userRouter} = require("./routers/userRouter.js");
+const { userRouter } = require("./routers/userRouter.js");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const router = express.Router();
-
-const port = 8080;
 
 router.use("/", mapRouter);
 router.use("/", dogRouter);
@@ -25,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/api/auth", userRouter);
 app.listen(8080, function () {
-	console.log("severOpen");
+  console.log("severOpen");
 });
 
 const DB_URL = process.env.ATLAS_URL;
