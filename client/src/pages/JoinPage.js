36f3e0a8d6bec79e301c9fc4a9d95757cbdfacd3
Song eunji <ckacl369@gmail.com>
2023-12-22 23:06:08 +0900

    :sparkles: 펫 페이지 제작 하는 중

--- client/src/pages/JoinPage.js
+++ client/src/pages/JoinPage.js
@@ -1,24 +1,24 @@
-import React, { useCallback, useState } from "react";
-import { LogoBar, NavBar } from "../components/common/Header";
-import { LongColoredBtn, SmallBtn } from "../components/common/Buttons";
-import { ContainerBox, InputBox } from "../components/common/Boxes"; // Removed StyledContainerBox
-import styled from "styled-components";
-import axios from "axios";
+import React, { useCallback, useState } from 'react';
+import { LogoBar, NavBar } from '../components/common/Header';
+import { LongColoredBtn, SmallBtn } from '../components/common/Buttons';
+import { ContainerBox, InputBox } from '../components/common/Boxes'; // Removed StyledContainerBox
+import styled from 'styled-components';
+import axios from 'axios';
 
 function JoinPage() {
-  const [nickname, setNickname] = useState("");
-  const [user_Id, setUser_Id] = useState("");
-  const [email, setEmail] = useState("");
-  const [number, setNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [CheckPassword, setCheckPassword] = useState("");
-  const [pwdMsg, setPwdMsg] = useState("");
-  const [confirmPwdMsg, setConfirmPwdMsg] = useState("");
-  const [nicknameCheckMsg, setNicknameCheckMsg] = useState("");
-  const [UserIdCheckMsg, setUserIdCheckMsg] = useState("");
-  const [EmailCheckMsg, setEmailCheckMsg] = useState("");
-  const [NumberCheckMsg, setNumberCheckMsg] = useState("");
-  const [StayNumber, setStayNumber] = useState("");
+  const [nickname, setNickname] = useState('');
+  const [user_Id, setUser_Id] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [CheckPassword, setCheckPassword] = useState('');
+  const [pwdMsg, setPwdMsg] = useState('');
+  const [confirmPwdMsg, setConfirmPwdMsg] = useState('');
+  const [nicknameCheckMsg, setNicknameCheckMsg] = useState('');
+  const [UserIdCheckMsg, setUserIdCheckMsg] = useState('');
+  const [EmailCheckMsg, setEmailCheckMsg] = useState('');
+  const [NumberCheckMsg, setNumberCheckMsg] = useState('');
+  const [StayNumber, setStayNumber] = useState('');
 
   const handleChangeNickname = (e) => {
     setNickname(e.target.value);
@@ -27,23 +27,23 @@ function JoinPage() {
   const handleClickNickname = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/auth/check-nickname",
+        'http://localhost:8080/api/auth/check-nickname',
         {
           nickName: nickname,
-        }
+        },
       );
 
       console.log(response);
       console.log(nickname); // 확인용 로그
 
       if (response.data.check === false) {
-        setNicknameCheckMsg("중복된 닉네임입니다.");
+        setNicknameCheckMsg('중복된 닉네임입니다.');
       } else {
-        setNicknameCheckMsg("사용 가능한 닉네임입니다.");
+        setNicknameCheckMsg('사용 가능한 닉네임입니다.');
       }
-      console.log("닉네임 중복체크");
+      console.log('닉네임 중복체크');
     } catch (error) {
-      console.error("API 호출 중 에러 발생:", error);
+      console.error('API 호출 중 에러 발생:', error);
     }
   };
 
@@ -55,21 +55,21 @@ function JoinPage() {
     console.log(typeof user_Id);
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/auth/check-id",
+        'http://localhost:8080/api/auth/check-id',
         {
           userId: user_Id,
-        }
+        },
       );
 
       console.log(response);
       if (response.data.check === false) {
-        setUserIdCheckMsg("중복된 아이디입니다.");
+        setUserIdCheckMsg('중복된 아이디입니다.');
       } else {
-        setUserIdCheckMsg("사용 가능한 아이디 입니다.");
+        setUserIdCheckMsg('사용 가능한 아이디 입니다.');
       }
-      console.log("아이디 중복체크");
+      console.log('아이디 중복체크');
     } catch (error) {
-      console.error("API 호출 중 에러 발생:", error);
+      console.error('API 호출 중 에러 발생:', error);
     }
   };
 
@@ -81,22 +81,22 @@ function JoinPage() {
     console.log(email);
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/auth/check-email",
+        'http://localhost:8080/api/auth/check-email',
         {
           email: email,
-        }
+        },
       );
       if (response.status === 200) {
         if (response.data === null) {
-          setEmailCheckMsg("중복된 이메일입니다.");
+          setEmailCheckMsg('중복된 이메일입니다.');
         } else {
-          setEmailCheckMsg("해당 이메일로 인증번호를 보냈습니다.");
+          setEmailCheckMsg('해당 이메일로 인증번호를 보냈습니다.');
           setStayNumber(response.data.data.authNumber);
         }
-        console.log("ok");
+        console.log('ok');
       }
     } catch (error) {
-      console.error("API 호출 중 에러 발생:", error);
+      console.error('API 호출 중 에러 발생:', error);
     }
   };
 
@@ -108,10 +108,10 @@ function JoinPage() {
     console.log(number);
     console.log(StayNumber);
     if (Number(number) === Number(StayNumber)) {
-      setNumberCheckMsg("인증번호가 확인되었습니다.");
-      console.log("ok");
+      setNumberCheckMsg('인증번호가 확인되었습니다.');
+      console.log('ok');
     } else {
-      setNumberCheckMsg("인증번호가 틀렸습니다.");
+      setNumberCheckMsg('인증번호가 틀렸습니다.');
     }
   };
 
@@ -126,14 +126,14 @@ function JoinPage() {
     const currPassword = e.target.value;
     setPassword(currPassword);
 
-    if (currPassword !== "") {
+    if (currPassword !== '') {
       if (!PasswordValid(currPassword)) {
-        setPwdMsg("영문, 숫자가 포함된 6자리 이상");
+        setPwdMsg('영문, 숫자가 포함된 6자리 이상');
       } else {
-        setPwdMsg("안전한 비밀번호입니다.");
+        setPwdMsg('안전한 비밀번호입니다.');
       }
     } else {
-      setPwdMsg("");
+      setPwdMsg('');
     }
   }, []);
 
@@ -142,17 +142,17 @@ function JoinPage() {
       const CheckPassword = e.target.value;
       setCheckPassword(CheckPassword);
 
-      if (CheckPassword !== "") {
+      if (CheckPassword !== '') {
         if (CheckPassword !== password) {
-          setConfirmPwdMsg("비밀번호가 일치하지 않습니다.");
+          setConfirmPwdMsg('비밀번호가 일치하지 않습니다.');
         } else {
-          setConfirmPwdMsg("비밀번호가 일치합니다.");
+          setConfirmPwdMsg('비밀번호가 일치합니다.');
         }
       } else {
-        setConfirmPwdMsg(""); // 입력이 비어 있을 때 오류 메시지를 지웁니다.
+        setConfirmPwdMsg(''); // 입력이 비어 있을 때 오류 메시지를 지웁니다.
       }
     },
-    [password]
+    [password],
   );
 
   const handleJoin = async () => {
@@ -165,51 +165,51 @@ function JoinPage() {
       !password ||
       !CheckPassword
     ) {
-      console.error("모든 항목을 입력해주세요.");
+      console.error('모든 항목을 입력해주세요.');
       return;
     }
 
     // 중복 체크 여부 확인
-    if (nicknameCheckMsg !== "사용 가능한 닉네임입니다.") {
-      console.error("닉네임이 유효하지 않습니다.");
+    if (nicknameCheckMsg !== '사용 가능한 닉네임입니다.') {
+      console.error('닉네임이 유효하지 않습니다.');
       return;
     }
 
-    if (UserIdCheckMsg !== "사용 가능한 아이디 입니다.") {
-      console.error("아이디가 유효하지 않습니다.");
+    if (UserIdCheckMsg !== '사용 가능한 아이디 입니다.') {
+      console.error('아이디가 유효하지 않습니다.');
       return;
     }
 
-    if (EmailCheckMsg !== "해당 이메일로 인증번호를 보냈습니다.") {
-      console.error("이메일 또는 이메일 인증이 유효하지 않습니다.");
+    if (EmailCheckMsg !== '해당 이메일로 인증번호를 보냈습니다.') {
+      console.error('이메일 또는 이메일 인증이 유효하지 않습니다.');
       return;
     }
 
-    if (NumberCheckMsg !== "인증번호가 확인되었습니다.") {
-      console.error("인증번호가 유효하지 않습니다.");
+    if (NumberCheckMsg !== '인증번호가 확인되었습니다.') {
+      console.error('인증번호가 유효하지 않습니다.');
       return;
     }
 
     // 비밀번호 일치 여부 확인
     if (
-      pwdMsg !== "안전한 비밀번호입니다." ||
-      confirmPwdMsg !== "비밀번호가 일치합니다."
+      pwdMsg !== '안전한 비밀번호입니다.' ||
+      confirmPwdMsg !== '비밀번호가 일치합니다.'
     ) {
-      console.error("비밀번호 또는 비밀번호 확인이 유효하지 않습니다.");
+      console.error('비밀번호 또는 비밀번호 확인이 유효하지 않습니다.');
       return;
     }
 
     // 회원가입 요청
     try {
-      await axios.post("http://localhost:8080/api/auth/sign-up", {
+      await axios.post('http://localhost:8080/api/auth/sign-up', {
         email,
         userId: user_Id,
         nickName: nickname,
         password,
       });
-      console.log("회원가입 성공");
+      console.log('회원가입 성공');
     } catch (error) {
-      console.error("회원가입에 실패했습니다.", error);
+      console.error('회원가입에 실패했습니다.', error);
     }
   };
 
@@ -335,7 +335,7 @@ function JoinPage() {
             <div className="message">{confirmPwdMsg}</div>
           </div>
           <div className="btn-box" onClick={handleJoin}>
-            <LongColoredBtn text={"가입하기"} className="long-btn" />
+            <LongColoredBtn text={'가입하기'} className="long-btn" />
           </div>
         </JoinContents>
       </Main>
--- client/src/pages/MyPetPage.js
+++ client/src/pages/MyPetPage.js
@@ -1,11 +1,82 @@
-import React, { useCallback, useState } from "react";
-import { LogoBar, NavBar } from "../components/common/Header";
-import { LongColoredBtn, SmallBtn } from "../components/common/Buttons";
-import { ContainerBox, InputBox } from "../components/common/Boxes"; // Removed StyledContainerBox
-import styled from "styled-components";
-import axios from "axios";
+import { useState } from 'react';
+import { LogoBar, NavBar } from '../components/common/Header';
+import styled from 'styled-components';
+import ProfileComponent from '../components/myPetPage/ProfileComponent';
+import WeightComponent from '../components/myPetPage/WeightComponent';
+import MedicalComponent from '../components/myPetPage/MedicalComponent';
+import FoodComponent from '../components/myPetPage/FoodComponent';
+import MemoComponent from '../components/myPetPage/MemoComponent';
 
 function MyPetPage() {
-  return <div></div>;
+  const [tab, setTab] = useState('profile');
+
+  function handleClickTab() {}
+
+  return (
+    <PetContainer>
+      <LogoBar />
+      <NavBar />
+      <Main>
+        <div className="title">의 페이지</div>
+        <PetDiaryContainer>
+          <div className="tab-container">
+            <div
+              onClick={() => setTab('profile')}
+              className={tab === 'profile' ? 'clicked' : ''}
+            >
+              프로필
+            </div>
+            <div
+              onClick={() => setTab('weight')}
+              className={tab === 'weight' ? 'clicked' : ''}
+            >
+              몸무게
+            </div>
+            <div
+              onClick={() => setTab('medical')}
+              className={tab === 'medical' ? 'clicked' : ''}
+            >
+              진료 기록
+            </div>
+            <div
+              onClick={() => setTab('food')}
+              className={tab === 'food' ? 'clicked' : ''}
+            >
+              사료/영양제/간식
+            </div>
+            <div
+              onClick={() => setTab('memo')}
+              className={tab === 'memo' ? 'clicked' : ''}
+            >
+              메모
+            </div>
+          </div>
+          <div className="content-container">
+            {isProfileClick && <ProfileComponent />}
+            {isWeightClick && <WeightComponent />}
+            {isMedicalClick && <MedicalComponent />}
+            {isFoodClick && <FoodComponent />}
+            {isMemoClick && <MemoComponent />}
+          </div>
+        </PetDiaryContainer>
+      </Main>
+    </PetContainer>
+  );
 }
+
 export default MyPetPage;
+
+const PetContainer = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PetDiaryContainer = styled.div`
+  display: flex;
+`;
