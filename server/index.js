3a0b6eefa3dde288331fee53195a48ae650edb24
Normaljun95 <jsj950629@gmail.com>
2023-12-23 02:09:57 +0900

    ✨Feat: 쿠키에서 로컬스토리지로

--- server/index.js
+++ server/index.js
@@ -19,13 +19,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
