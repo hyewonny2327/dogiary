e58ed3d35a0c2fba3b70fcbba419d1677587332d
[SW7] 윤혜원 <wonny2327@gmail.com>
2023-12-23 22:28:38 +0900

    :twisted_rightwards_arrows: Merge with dev

--- server/routers/diaryRouter.js
+++ server/routers/diaryRouter.js
@@ -4,12 +4,23 @@ const diaryController = require("../controllers/diaryController");
 // 사용자 인증을 위한 미들웨어
 const authenticateUser = require("../middlewares/authenticateUser");
 const router = Router();
+const { upload } = require("../utils/multer.js");
 
 //일기 생성
-router.post("/", authenticateUser, diaryController.postDiary);
+router.post(
+  "/",
+  authenticateUser,
+  upload.single("imageUrl"),
+  diaryController.postDiary
+);
 
 //일기 수정
-router.put(`/:id`, authenticateUser, diaryController.putDiary);
+router.put(
+  `/:id`,
+  authenticateUser,
+  upload.single("imageUrl"),
+  diaryController.putDiary
+);
 
 //일기 삭제
 router.delete(`/:id`, authenticateUser, diaryController.deleteDiary);
@@ -21,6 +32,6 @@ router.get("/", authenticateUser, diaryController.getDiaries);
 router.get("/month", authenticateUser, diaryController.getMonthDiaries);
 
 //커서 기반 페이징
-router.get("/paging", authenticateUser, diaryController.getCurosrDiaries);
+router.get("/paging", authenticateUser, diaryController.getCursorDiaries);
 
 module.exports = router;
