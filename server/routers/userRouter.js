1cf0ff4804e782ccd83687eb92cc5861510e851f
Normaljun95 <jsj950629@gmail.com>
2023-12-25 23:33:31 +0900

    ✨Feat: user validator 구현

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const authenticateUser = require('../middlewares/authenticateUser.js');
 const userController = require('../controllers/userController');
-const { validateSignUp, validateSignIn } = require('../middlewares/validator');
+const { validateSignUp, validateSignIn, validateUpdateUserInfo, validateCheckPassword, validateCheckUserId, validateCheckNickname, validateCheckEmail } = require('../middlewares/validator');
 const userRouter = express.Router();
 const { upload } = require('../utils/multer.js');
 
@@ -22,6 +22,7 @@ userRouter.get('/my-page', authenticateUser, userController.getUserInformation);
 userRouter.put(
   '/my-page',
   authenticateUser,
+  validateUpdateUserInfo,
   upload.single('imageUrl'),
   userController.updateUserInformation,
 );
@@ -37,22 +38,23 @@ userRouter.delete(
 userRouter.post(
   '/check-password',
   authenticateUser,
+  validateCheckPassword,
   userController.checkUserPassword,
 );
 
 //아이디 중복 확인
-userRouter.post('/check-id', userController.checkUserId);
+userRouter.post('/check-id', validateCheckUserId, userController.checkUserId);
 
 //닉네임 중복 확인
-userRouter.post('/check-nickname', userController.checkUserNickname);
+userRouter.post('/check-nickname', validateCheckNickname, userController.checkUserNickname);
 
 //이메일 인증 (feat.이메일 중복확인)
-userRouter.post('/check-email', userController.checkUserEmail);
+userRouter.post('/check-email', validateCheckEmail, userController.checkUserEmail);
 
-//임시 비밀번호 발급
-userRouter.post('/help', userController.sendTemporaryPassword);
+//임시 비밀번호 발급(비밀번호 찾기)
+userRouter.post('/help', validateCheckEmail, userController.sendTemporaryPassword);
 
 //아이디 찾기
-userRouter.post('/find-id', userController.sendUserId);
+userRouter.post('/find-id', validateCheckEmail, userController.sendUserId);
 
 module.exports = userRouter;
