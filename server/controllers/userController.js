9bd230260b590588b33e3f0560ec740e8738937c
Normaljun95 <jsj950629@gmail.com>
2023-12-27 15:31:35 +0900

    💡Comment: User 불필요한 주석 제거

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -21,7 +21,6 @@ const userController = {
       const loginData = req.body;
       const result = await userService.signIn(loginData);
 
-      // res.cookie("Authorization", `Bearer ${result.token}`, { httpOnly: true });
       res.status(200).json({
         error: null,
         data: result,
@@ -34,10 +33,9 @@ const userController = {
   //로그아웃
   async signOutUser(req, res, next) {
     try {
-      // const userToken = req.cookies.Authorization?.split(" ")[1] ?? "null";
-      // const userToken = req.headers.cookie.split("%20")[1] ?? "null";
       const userToken = req.headers.authorization || null;
-      const result = await userService.signOut(userToken);
+      const tokenValue = userToken.split('Bearer ')[1];
+      const result = await userService.signOut(tokenValue);
 
       // res.clearCookie("Authorization");
       res.status(204).send();
@@ -110,7 +108,6 @@ const userController = {
 
       const result = await userService.deleteUserInfo(userId);
 
-      // res.clearCookie("Authorization");
       res.status(204).send();
     } catch (error) {
       next(error);
